4a2e8b9dddc2770ea157e22992adee929692170c
Carlos Padilla <mr.carlos.padilla@gmail.com>
2020-12-17 16:50:24 -0600

    updated tests and code

--- code.js
+++ code.js
@@ -3,6 +3,9 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello() {
-//empty for now...
+function sayHello(input) {
+    if(input === "Alex") {
+        return "Hello, Alex!";
+    } else { return "Hello, Jane!"
+    }
 }
