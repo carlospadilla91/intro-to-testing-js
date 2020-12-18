14aef2215eacab5676b587b53a3ef579c69502f3
Carlos Padilla <mr.carlos.padilla@gmail.com>
2020-12-17 22:45:49 -0600

    updated tests and code

--- code.js
+++ code.js
@@ -3,14 +3,14 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-// function sayHello(input) {
-//     if(input === "Alex") {
-//         return "Hello, Alex!";
-//     } else if(input === "Pat") {
-//         return "Hello, Pat!";
-//     } else { return "Hello, Jane!"
-//     }
-// }
+function sayHello(input) {
+    if(input === "Alex") {
+        return "Hello, Alex!";
+    } else if(input === "Pat") {
+        return "Hello, Pat!";
+    } else { return "Hello, Jane!"
+    }
+}
 
 function sayHello(input) {
     return "Hello, " + input + "!"
--- tests.js
+++ tests.js
@@ -19,7 +19,7 @@ describe('sayHello', function () {
         expect(typeof sayHello).toBe('function');
     });
     it('should return a string when called', function () {
-        expect(typeof sayHello()).toBe('string');
+        expect(typeof sayHello(true)).toBe('Hello, World');
     });
     it('should return the string "Hello, Jane!" when executed', function () {
         expect(sayHello('Jane')).toBe("Hello, Jane!");
@@ -28,6 +28,13 @@ describe('sayHello', function () {
         expect(sayHello('Alex')).toBe("Hello, Alex!");
     });
     it('should return the string "Hello, Pat!" when executed', function () {
-        expect(sayHello('Pat')).toBe("Hello, Pat!");
+        expect(sayHello('Pat')).toBe("Hello, Pat");
     });
 });
+
+describe('isFive', function () {
+    it('should be a defined function', function () {
+        expect(typeof isFive).toBe('function');
+    });
+});
+
